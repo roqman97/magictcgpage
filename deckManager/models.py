d0ae2f167560ed29b0ced3b154c474616b033aca
Taylor Rockey <tarockey@microsoft.com>
2018-04-23 16:17:32 -0700

    updated models

--- deckManager/models.py
+++ deckManager/models.py
@@ -8,10 +8,10 @@ class Card(models.Model):
     creature_type = models.CharField(max_length=250)
     losses = models.IntegerField()
     wins = models.IntegerField()
-    power = models.IntegerField()
+    power = models.CharField(max_length=10)
     quantity = models.IntegerField()
     title = models.TextField()
-    toughness = models.IntegerField()
+    toughness = models.CharField(max_length=10)
 
 class Deck(models.Model):
     colors = models.CharField(max_length=100)
@@ -23,8 +23,8 @@ class Deck(models.Model):
 class Deck_Cards(models.Model):
     card_id = models.ForeignKey(Card, on_delete=models.CASCADE)
     deck_id = models.ForeignKey(Deck, on_delete=models.CASCADE)
-    deck_revision_added = models.ForeignKey(Deck, on_delete=models.CASCADE)
-    deck_revision_removed = models.ForeignKey(Deck, on_delete=models.CASCADE)
+    deck_revision_added = models.IntegerField()
+    deck_revision_removed = models.IntegerField()
     wins = models.IntegerField()
     losses = models.IntegerField
     is_active = models.BooleanField()
